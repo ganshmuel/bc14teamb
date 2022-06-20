c4bd8ab9adc8094c8149601e461e6dbafa0ff875
Roei Keret <roei.keret@gmail.com>
2022-06-20 22:52:44 +0300

    more validations on the 'name' request param

--- billing/app/app.py
+++ billing/app/app.py
@@ -5,8 +5,16 @@ from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
+
+
+def non_empty_string(s):
+    if not s:
+        raise ValueError("Must not be empty string")
+    return s
+
+
 parser = reqparse.RequestParser()
-parser.add_argument('name', required=True, nullable=False)
+parser.add_argument('name', required=True, nullable=False, type=non_empty_string)
 
 
 class ProviderPost(Resource):
