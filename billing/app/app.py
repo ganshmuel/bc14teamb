99b35339b674b66cd5e40704e0b9e24445c59dd3
Amir Benyamini <amirbenyamini@Amirs-MacBook-Pro.local>
2022-06-20 19:56:05 +0300

    changed entry point to run app.py directly

--- billing/app/app.py
+++ billing/app/app.py
@@ -4,8 +4,7 @@ from flask import Flask, Response
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://billdb_app:CYuoYBprY4so@database-1.c62tpaoqyddb.us-east-1.rds.amazonaws.com:3621/billdb"
-# app.config["SERVER_NAME"] = "0.0.0.0"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://billdb_app:CYuoYBprY4so@database-1.c62tpaoqyddb.us-east-1.rds.amazonaws.com:3621/billdb"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
@@ -37,4 +36,4 @@ def provider(name):
     return dbRes
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
