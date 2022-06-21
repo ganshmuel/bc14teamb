5ec87da5771683d062cf95e629e602abbdc11453
Amir Benyamini <amirbenyamini@Amirs-MacBook-Pro.local>
2022-06-21 10:24:34 +0300

    connect to db,add docker-compose depends-on and restart always to app service, getting data in post routes

--- billing/app/app.py
+++ billing/app/app.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python3
 
-from flask import Flask, Response
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, Response, request
+from flask_restful import Resource, Api, reqparse
+import mysql.connector
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://billdb_app:CYuoYBprY4so@database-1.c62tpaoqyddb.us-east-1.rds.amazonaws.com:3621/billdb"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+api = Api(app)
+parser = reqparse.RequestParser()
+parser.add_argument('name', required=True)
 
-
-class Provider(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    providerName = db.Column(db.String(255), unique=True, nullable=False)
-
+mydb = mysql.connector.connect(
+    host="db",
+    user="root",
+    password="password",
+    database="billdb",
+    auth_plugin='mysql_native_password'
+    )
 
 # Routes Section
 def createProvider(name: str):
-    if name is None or name == "":
-        isProviderExist = Provider.query.filter_by(providerName=name).first()
-        if isProviderExist is None:
-            newProvider = Provider(providerName=name)
-            db.session.add(newProvider)
-            db.session.commit()
-            return Response({"id": newProvider.id}, status=200, mimetype='application/json')
-        else:
-            return Response('400 Bad Request: This provider name is already exist!', status=400, mimetype='text')
+    if name != None or name != "":
+        print("hello")
+        # isProviderExist = Provider.query.filter_by(providerName=name).first()
+        # if isProviderExist is None:
+        #     newProvider = Provider(providerName=name)
+        #     db.session.add(newProvider)
+        #     db.session.commit()
+        #     return Response({"id": newProvider.id}, status=200, mimetype='application/json')
+        # else:
+        #     return Response('400 Bad Request: This provider name is already exist!', status=400, mimetype='text')
     else:
         return Response('400 Bad Request: Please pass a valid provider name!', status=400, mimetype='text')
 
@@ -34,9 +38,10 @@ def health():
     return Response('OK', status=200, mimetype='text')
 
 
-@app.route("/provider/<name>", methods=["POST"])
-def provider(name):
-    dbRes = createProvider(name)
+@app.route("/provider", methods=["POST"])
+def provider():
+    data = request.json
+    dbRes = createProvider(data["name"])
     return dbRes
 
 
