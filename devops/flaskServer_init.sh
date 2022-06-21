7c515977a75dea0bb52281c16281fb0ee4b9fca9
devops <devops.gmail.com>
2022-06-21 10:34:01 +0000

    change name of flask-server to ci-server

--- devops/flaskServer_init.sh
+++ devops/flaskServer_init.sh
@@ -2,4 +2,4 @@
 
 docker stop flask-server
 docker build -t flask-server:v1 . && \
-docker run --rm -d --name flask-server -p 8090:5000 -v $(pwd)/../billing:/tests -v /var/run/docker.sock:/var/run/docker.sock  -v $(pwd)/server-config-files:/server flask-server:v1
+docker run --rm -d --name ci-server -p 8090:5000 -v $(pwd)/../billing:/tests -v /var/run/docker.sock:/var/run/docker.sock  -v $(pwd)/server-config-files:/server flask-server:v1
