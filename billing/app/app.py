db294c6b11d02d08bc3c732256733e34a4b96a3b
Amir Benyamini <amirbenyamini@Amirs-MacBook-Pro.local>
2022-06-21 10:58:22 +0300

    implement flask_restful libery on health route and post route

--- billing/app/app.py
+++ billing/app/app.py
@@ -6,8 +6,6 @@ import mysql.connector
 
 app = Flask(__name__)
 api = Api(app)
-parser = reqparse.RequestParser()
-parser.add_argument('name', required=True)
 
 mydb = mysql.connector.connect(
     host="db",
@@ -17,33 +15,35 @@ mydb = mysql.connector.connect(
     auth_plugin='mysql_native_password'
     )
 
+def non_empty_string(s):
+    if not s:
+        raise ValueError("Must not be empty string")
+    return s
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('name', required=True, nullable=False, type=non_empty_string)
+
 # Routes Section
 def createProvider(name: str):
     if name != None or name != "":
         print("hello")
-        # isProviderExist = Provider.query.filter_by(providerName=name).first()
-        # if isProviderExist is None:
-        #     newProvider = Provider(providerName=name)
-        #     db.session.add(newProvider)
-        #     db.session.commit()
-        #     return Response({"id": newProvider.id}, status=200, mimetype='application/json')
-        # else:
-        #     return Response('400 Bad Request: This provider name is already exist!', status=400, mimetype='text')
     else:
         return Response('400 Bad Request: Please pass a valid provider name!', status=400, mimetype='text')
 
 
-@app.route("/health", methods=["GET"])
-def health():
-    return Response('OK', status=200, mimetype='text')
+class HealthGet(Resource):
+    def get(self):
+        return Response('OK', status=200, mimetype='text')
 
+class ProviderPost(Resource):
+    def post(self):
+        args = parser.parse_args()
+        name = args['name']
+        return name
 
-@app.route("/provider", methods=["POST"])
-def provider():
-    data = request.json
-    dbRes = createProvider(data["name"])
-    return dbRes
-
+api.add_resource(HealthGet, '/', '/health')
+api.add_resource(ProviderPost, '/', '/provider')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
