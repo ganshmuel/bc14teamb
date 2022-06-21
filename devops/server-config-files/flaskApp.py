29c6b4743f4adeb57ef942dabd171b514379ebfc
Ben Dvilanski <dvilanski@gmail.com>
2022-06-21 14:24:44 +0300

    reading git hub posr req

--- devops/server-config-files/flaskApp.py
+++ devops/server-config-files/flaskApp.py
@@ -1,5 +1,5 @@
 from re import sub
-from flask import Flask
+from flask import Flask , request
 import subprocess
 
 app = Flask(__name__)
@@ -7,9 +7,17 @@ app = Flask(__name__)
 def runCompose():
     subprocess.run(["bash","run-compose.sh"])
 
+@app.route("/github-webhook", methods=['POST'])
+def githubWebhook():
+    
+    return "ok"
 
-@app.route("/test")
-def health():
+@app.route("/test", method=["GET", "POST"])
+def test():
+    content = request.json
+    branch = content['ref'].partition('refs/heads/')[2]
+    repository = content['repository']['full_name']
+    print(branch, repository)
     runCompose()
     return "OK"
 
