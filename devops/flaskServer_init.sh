0f037fa582bfe326e9cfabb5b5692734ff6bdc8a
Ben Dvilanski <dvilanski@gmail.com>
2022-06-21 12:06:39 +0300

    build flask server runnig compose from billing on <ip>:8090/test

--- devops/flaskServer_init.sh
+++ devops/flaskServer_init.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-
+docker stop flask-server
 docker build -t flask-server:v1 . && \
-docker run --rm -d --name test-server -p 8090:5000 -v $(pwd)/server-config-files:/server flask-server:v1
+docker run --rm -d --name flask-server -p 8090:5000 -v $(pwd)/../billing:/tests -v /var/run/docker.sock:/var/run/docker.sock  -v $(pwd)/server-config-files:/server flask-server:v1
