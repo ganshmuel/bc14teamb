e26c9a2656d868b7ac34032dec32b6dee90eb1f7
devops <devops.gmail.com>
2022-06-22 13:31:42 +0000

    change billing docker compose with image name

--- devops/exec-files/run-compose.sh
+++ devops/exec-files/run-compose.sh
@@ -3,7 +3,7 @@ BRANCH_NAME=$1
 ENV_TYPE=$2
 #(--env-file f"/test-env/bc14teamb/${BRANCH_NAME}/.env.${ENV_TYPE}")
 docker compose --env-file "/test-env/compose-config-files/.env.$ENV_TYPE"  \
- -f "/test-env/bc14teamb/$BRANCH_NAME/docker-compose.yml" up -d --build
+ -f "/test-env/bc14teamb/$BRANCH_NAME/docker-compose.yml" up -d 
 
 
 
