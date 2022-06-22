7a7ae571e9d7b2767bb3654667b7c634a18bfd57
devops <devops.gmail.com>
2022-06-22 12:54:32 +0000

    commit changes to test server

--- devops/flaskServer_init.sh
+++ devops/flaskServer_init.sh
@@ -2,7 +2,7 @@
 
 docker stop ci-server
 docker build -t flask-server:v1 . && \
-docker run  --rm -d --name ci-server -p 8090:5000 \
+docker run  --rm -d --name ci-test-server -p 8089:5000 \
 -v /var/run/docker.sock:/var/run/docker.sock  \
 -v $(pwd)/exec-files/:/test-env/exec-files \
 -v $(pwd)/key:/root/.ssh \
