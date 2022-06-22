39dc8c3fbf0535cbb853db58c0e5fdf34b81af07
Roei Keret <roei.keret@gmail.com>
2022-06-22 18:35:54 +0300

    fixed tests

--- tests/checks.sh
+++ tests/checks.sh
@@ -3,6 +3,7 @@
 declare provider_name
 declare response_code
 declare host="ec2-3-88-220-120.compute-1.amazonaws.com"
+declare port=8080
 
 function check_response_code {
   if [ $1 -ne $2 ] ; then
@@ -10,20 +11,24 @@ function check_response_code {
 	echo "RESULT: Expected code $1, got code $2"
 	exit 1
   else
+	echo "RESULT: Pass"
 	return 0
   fi
 }
 
 function generate_provider_name {
-	provider_name=$RANDOM;
+	provider_name="$(echo $RANDOM | md5sum | head -c 20)";
 }
 
+function generate_provider_name_int {
+	provider_name="$RANDOM";
+}
 
 # Create a random provider and return its ID
 function create_provider {
 	generate_provider_name
 	payload="$(jq --null-input --arg nm "$provider_name" '{"name": $nm}')"
-	provider_id="$(curl -s -H 'Content-Type: application/json' --data "$payload" http://$host:8080/provider/ | jq -r .id)"
+	provider_id="$(curl -s -H 'Content-Type: application/json' --data "$payload" http://$host:$port/provider/ | jq -r .id)"
 	echo $provider_id
 }
 
@@ -36,7 +41,7 @@ generate_provider_name
 # Preapre body
 payload="$(jq --null-input --arg nm "$provider_name" '{"name": $nm}')"
 # Prepare URL
-url="http://$host:8080/provider/"
+url="http://$host:$port/provider/"
 # Send request
 response_code="$(curl -o /dev/null -s -w "%{http_code}\n" -H "Content-Type: application/json" --data "$payload" $url)"
 # Test response
@@ -51,18 +56,20 @@ check_response_code "400" "${response_code}"
 
 # ----------- PUT /Provider -----------
 
-echo "TEST: PUT /Provider, positive test"
+echo "TEST: PUT /Provider, positive test, change name to alphabetics"
 
 # Create a random provider
 provider_id=$(create_provider)
+echo "LOG: Created new provider with ID $provider_id"
 # Generate new name for existing provider
 generate_provider_name
 # Preapre body
 payload="$(jq --null-input --arg nm "$provider_name" '{"name": $nm}')"
 # Prepare URL
-url="http://$host:8080/provider/$provider_id)"
+url="http://$host:$port/provider/$provider_id"
 # Send request
 response_code="$(curl -X PUT -o /dev/null -s -w "%{http_code}\n" -H "Content-Type: application/json" --data "$payload" $url)"
+echo "LOG: Changed provider ID $provider_id name to $provider_name"
 # Test response
 check_response_code "200" "${response_code}"
 
