e122f7c444926d547f5f7d510aa4864a3de1417b
devops <devops.gmail.com>
2022-06-22 14:50:19 +0000

    commit before mergin devops

--- devops/exec-files/run-compose.sh
+++ devops/exec-files/run-compose.sh
@@ -2,7 +2,7 @@
 BRANCH_NAME=$1
 ENV_TYPE=$2
 #(--env-file f"/test-env/bc14teamb/${BRANCH_NAME}/.env.${ENV_TYPE}")
-docker compose --env-file "/test-env/compose-config-files/.env.$ENV_TYPE"  \
+docker compose -p ${BRANCHNAME}${ENV_TYPE} --env-file "/test-env/compose-config-files/.env.$ENV_TYPE"  \
  -f "/test-env/bc14teamb/$BRANCH_NAME/docker-compose.yml" up -d 
 
 
