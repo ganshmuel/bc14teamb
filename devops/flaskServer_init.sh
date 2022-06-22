6dfa9dc046901e46212b1b174a449ad724d5ffa1
devops <devops.gmail.com>
2022-06-22 13:00:01 +0000

    create test ci server

--- devops/flaskServer_init.sh
+++ devops/flaskServer_init.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-docker stop ci-server
+docker stop ci-test-server
 docker build -t flask-server:v1 . && \
 docker run  --rm -d --name ci-test-server -p 8089:5000 \
 -v /var/run/docker.sock:/var/run/docker.sock  \
