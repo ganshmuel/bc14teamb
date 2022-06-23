e2dc6047ab748d297cead5dc73c19a0b05e44f85
Roei Keret <roei.keret@gmail.com>
2022-06-23 19:26:06 +0300

    fixed POST /truck/ and PUT /trucks/ tests

--- tests/checks.sh
+++ tests/checks.sh
@@ -56,6 +56,21 @@ function create_provider {
 	echo $provider_id
 }
 
+
+# Get random provider and truck by given truck license plate, return response status code
+function create_truck {
+    declare license_plate="$1"
+	# Create a random provider
+	provider_id=$(create_provider)
+	# Prepare URL
+	url=" http://$host:$port/truck/"
+	# Preapre body
+	payload="$(jq --null-input --arg pid "$provider_id" '{"provider_id": $pid, "id": "$license_plate"}')"
+	# Send request
+	response_code="$(curl -o /dev/null -s -w "%{http_code}\n" -H "Content-Type: application/json" --data "$payload" $url)"
+	echo $response_code
+}
+
 # ----------- POST /Provider -----------
 
 echo "TEST: POST /Provider, positive test"
@@ -98,26 +113,40 @@ echo "LOG: Changed provider ID $provider_id name to $provider_name"
 # Test response
 check_response_code "200" "${response_code}"
 
-# ----------- POST /Rates -----------
+# ----------- POST /truck -----------
 
+echo "TEST: POST /truck, positive test"
 
+# Generate a random truck's license plate
+truck_id="$RANDOM"
+# Create provider and truck
+response_code="$(create_truck $truck_id)"
+# Validate result
+check_response_code "200" "${response_code}"
 
 # ----------- PUT /trucks/<truck_id> -----------
 
 echo "TEST: PUT /trucks/<truck_id>, positive test"
 
-# carate a random provider and truck id
-provider_id=$(create_provider)
+# Create truck
+
+# Generate a random truck's license plate
 truck_id="$RANDOM"
-payload="$(jq --null-input --arg nm "$truck_id" '{"id": $nm , "provider_id": '$provider_id'}')"
-curl -s -H 'Content-Type: application/json' --data "$payload" http://$host:$port/truck/
+# Create provider and truck
+response_code="$(create_truck $truck_id)"
+# Validate result
+check_response_code "200" "${response_code}"
+
+# Update truck
 
 # Prepare URL
 url=" http://$host:$port/trucks/$truck_id"
-
+# Create new provider
+provider_id=$(create_provider)
+# Preapre body
+payload="$(jq --null-input --arg pid "$provider_id" '{"provider_id": $pid}')"
 # Send request
-response_code="$(curl -X PUT -o /dev/null -s -w "%{http_code}\n" -H "Content-Type: application/json" --data {\"provider_id\":\"$provider_id\"} $url)" 
-
+response_code="$(curl -X PUT -o /dev/null -s -w "%{http_code}\n" -H "Content-Type: application/json" --data "$payload" $url)" 
 # Test response
 check_response_code "200" "${response_code}"
 
