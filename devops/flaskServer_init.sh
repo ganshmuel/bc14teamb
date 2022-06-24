6850959081a1116b6f9e726cc93cad430c6ce86c
devops <devops.gmail.com>
2022-06-24 18:40:27 +0000

    fixed bug in deploy of new ci server

--- devops/flaskServer_init.sh
+++ devops/flaskServer_init.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 docker stop ci-server
 docker build -t ci-server:v2 . && \
-docker run --rm -d --name ci-test-server -p ci-server 8080:5000 \
+docker run --rm -d --name ci-test-server -p 8080:5000 \
 -v /var/run/docker.sock:/var/run/docker.sock  \
 -v $(pwd)/exec-files/:/test-env/exec-files \
 -v ~/.ssh:/root/.ssh \
