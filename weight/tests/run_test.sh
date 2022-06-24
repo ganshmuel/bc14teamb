a55bdd60dcce0992e89bef0e45a86ce32dcae72c
Matho Avito <matavital13@gmail.com>
2022-06-25 01:16:46 +0300

    changed localhost:8081 into  for dev tests

--- weight/tests/run_test.sh
+++ weight/tests/run_test.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
-
 # curl -H "Content-Type: application/json" -X POST -d '{"username":"mkyong","password":"abc"}' http://localhost:8080/api/login/
+
+curl="curl -s -I "localhost:8081"/health | grep HTTP/ | awk {'print '}"
 result="true"
-curl="curl -s -I localhost:8081/health | grep HTTP/ | awk {'print '}"
-result="true"
-declare -a routes=("health" "weight" "unknown/")
+declare -a routes=("health" "weight" "unknown/")  # an array of the working api's
 
 for api in "${routes[@]}"
     do
-    i=$(curl -s -I localhost:8081/"$api" | grep HTTP/ | awk {'print '})
+    i=$(curl -s -I "$1"/"$api" | grep HTTP/ | awk {'print '})   # "$1" is for dev team tests!!!
     if [[ $i != *"HTTP/1.0 200 OK"* ]]
             then
             result="false"
         fi
     done
 
-
 echo $result > log-test.txt
-
-
-
-# for api in ${routes}
-#     do
-#     $api $api
-#     echo $result
-#     done
