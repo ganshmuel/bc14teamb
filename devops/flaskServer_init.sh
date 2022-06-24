161adc66d577ee2973c8206df7aedd09db9970dc
devops <devops.gmail.com>
2022-06-24 18:38:35 +0000

    commit before deploing new ci server

--- devops/flaskServer_init.sh
+++ devops/flaskServer_init.sh
@@ -1,9 +1,9 @@
 #/bin/bash
-docker stop ci-test-server
-docker build -t flask-server:v1 . && \
-docker run --rm -d --name ci-test-server -p 8089:5000 \
+docker stop ci-server
+docker build -t ci-server:v2 . && \
+docker run --rm -d --name ci-test-server -p ci-server 8080:5000 \
 -v /var/run/docker.sock:/var/run/docker.sock  \
 -v $(pwd)/exec-files/:/test-env/exec-files \
--v $(pwd)/key:/root/.ssh \
+-v ~/.ssh:/root/.ssh \
 -v $(pwd)/compose-config-files/:/test-env/compose-config-files \
 -v $(pwd)/server-config-files:/server flask-server:v1
