5909b723e8b9e08da28c36102f94b6bcb59344e2
Ben Dvilanski <dvilanski@gmail.com>
2022-06-25 23:53:32 +0300

    .

--- devops/exec-files/run-production.sh
+++ devops/exec-files/run-production.sh
@@ -10,8 +10,9 @@ git config --global pull.rebase false
 cd /test-env/bc14teamb
 git checkout $BRANCH_NAME --
 git pull origin main
-git push origin main
 git checkout main
+git pull origin $BRANCH_NAME
+git push -f origin main
 docker compose --env-file "/test-env/compose-config-files/.env.${BRANCH_NAME}.prod"  \
 -f "/test-env/bc14teamb/${BRANCH_NAME}/docker-compose.yml" -p "${BRANCH_NAME}prod" up -d --build
 cd /test-env
