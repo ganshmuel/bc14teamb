32ae591ade23f4deabaebca667c4dd4b28a59d7b
Ben Dvilanski <dvilanski@gmail.com>
2022-06-26 10:18:56 +0300

    fixed bug in volume ssh

--- devops/flaskServer_init.sh
+++ devops/flaskServer_init.sh
@@ -5,6 +5,6 @@ docker run --rm -d --name ci-server -p 8090:5000 \
 -v /var/run/docker.sock:/var/run/docker.sock  \
 -v /test-env:/test-env \
 -v $(pwd)/exec-files/:/test-env/exec-files \
--v $(pwd)/key/:/root/.ssh \
+-v /home/ubuntu/.ssh:/root/.ssh \
 -v $(pwd)/compose-config-files/:/test-env/compose-config-files \
 -v $(pwd)/server-config-files:/server ci-server:v2
